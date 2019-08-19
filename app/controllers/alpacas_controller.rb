3ad02319ba0bec9205d862e0bab44f800038d67b
Fabian Schulze Wierling <fabian.schuwie@gmail.com>
2019-08-19 18:14:07 +0200

    added alpaca controller actions and seeding file for both alpacas and users as well as index view for alpacas

--- app/controllers/alpacas_controller.rb
+++ app/controllers/alpacas_controller.rb
@@ -1,22 +1,44 @@
 class AlpacasController < ApplicationController
   def index
+    @alpacas = Alpaca.all
   end
 
   def show
+    @alpaca = Alpaca.find(params[:id])
   end
 
   def new
+    @alpaca = Alpaca.new
   end
 
   def create
+    @alpaca = Alpaca.new(alpaca_params)
+    if @alpaca.save
+      redirect_to alpacas_path(@alpaca)
+    else
+      render :new
+    end
   end
 
   def edit
+    @alpaca = Alpaca.find(params[:id])
   end
 
   def update
+    @alpaca = Alpaca.find(params[:id])
+    @alpaca.update(alpaca_params)
+    redirect_to @alpaca
   end
 
   def destroy
+    @alpaca = Alpaca.find(params[:id])
+    @alpaca.destroy
+    redirect_to alpacas_path
+  end
+
+  private
+
+  def alpaca_params
+    params.require(:alpaca).permit(:name, :age, :address, :color, :craziness_level, :price, :user_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
 puts 'Cleaning current database initiated...'
+Alpaca.destroy_all
 User.destroy_all
-puts "#{User.count} user records have irreversibly been deleted from the database!"
+puts "#{User.count} user records and #{Alpaca.count} alpacas have irreversibly been deleted from the database!"
+puts 'Seeding initiated. Populating the database with new test users ...'
 
-puts 'Seeding initiated...'
-puts 'Populating the database with new test users...'
-
-100.times do |user|
-  user = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , email: Faker::Internet.email, password: "login123")
+10.times do |user|
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name ,
+    email: Faker::Internet.email,
+    password: "login123")
   user.save!
 end
 
-puts "Finished! Your database now has #{User.count} user records. Enjoy testing."
+puts "Successful seeding of test users. Now, only alpacas are missing."
+puts "Seeding initiated. Populating the database with new alpacas ..."
+
+10.times do |alpaca|
+  alpaca = Alpaca.new(
+      {
+        name: Faker::Name.first_name,
+        age: Faker::Number.within(range: 1..5),
+        address: Faker::Address.city,
+        color: Faker::Color.color_name,
+        craziness_level: 3,
+        price: Faker::Number.within(range: 3000..8000),
+        user: [User.first, User.second, User.third, User.fourth].sample
+      }
+    )
+  alpaca.save!
+end
+
+puts "Finished giving birth to #{Alpaca.count} new baby alpacas and creating #{User.count} new user records. Enjoy testing!"
