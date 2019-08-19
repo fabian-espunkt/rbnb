7db05b486fbc9cb3ba53370ea20ebea7de1ad2b6
Fabian Schulze Wierling <fabian.schuwie@gmail.com>
2019-08-19 16:08:53 +0200

    changed routes to be nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
-
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/show'
-  get 'alpacas/index'
-  get 'alpacas/show'
-  get 'alpacas/new'
-  get 'alpacas/create'
-  get 'alpacas/edit'
-  get 'alpacas/update'
-  get 'alpacas/destroy'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :alpacas do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
 end
