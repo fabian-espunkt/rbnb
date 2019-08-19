04e23466bd90dd0f450cddd26c9d561204ca988c
Fabian Schulze Wierling <fabian.schuwie@gmail.com>
2019-08-19 17:21:45 +0200

    solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning current database initiated...'
+User.destroy_all
+puts "#{User.count} user records have irreversibly been deleted from the database!"
+
+puts 'Seeding initiated...'
+puts 'Populating the database with new test users...'
+
+100.times do |user|
+  user = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , email: Faker::Internet.email, password: "login123")
+  user.save!
+end
+
+puts "Finished! Your database now has #{User.count} user records. Enjoy testing."
