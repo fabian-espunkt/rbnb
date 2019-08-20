0a47c3e1d2aded82689c7b686949ce5fc388075e
Simone Hoffmann <simohoff@gmx.de>
2019-08-20 14:15:04 +0200

    added method to reviewcontroller and started to create the review view with form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,18 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def show
-  end
+  private
+
+  booking_params.require(:booking).permit(:user_id, :alpaca_id, :review_id)
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+  review_params.require(:booking).permit(:content, :stars)
 end
