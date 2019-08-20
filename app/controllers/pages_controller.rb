0a5a8e56d1392e1e6dfd29dae310eac9221c4ba0
Fabian Schulze Wierling <fabian.schuwie@gmail.com>
2019-08-20 15:07:16 +0200

    implemented the functionality that you can now create a booking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def dashboard
+    @booking = Booking.where(user_id: current_user.id)
+  end
 end
