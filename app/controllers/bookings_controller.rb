d952a169bef2f250278f92a96a42c78431f49bf6
Simone Hoffmann <simohoff@gmx.de>
2019-08-20 18:31:32 +0200

    changed somehting in the controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -20,3 +20,17 @@ class BookingsController < ApplicationController
   def show
   end
 end
+
+
+
+  # def create
+  #   @plant = Plant.find(params[:plant_id])
+  #   @tags = Tag.find(params[:plant_tag][:tag])
+  #   @tags.each do |tag|
+  #     @plant_tag = PlantTag.new
+  #     @plant_tag.plant = @plant
+  #     @plant_tag.tag = tag
+  #     @plant_tag.save
+  #   end
+  #   redirect_to garden_path(@plant.garden)
+  # end
