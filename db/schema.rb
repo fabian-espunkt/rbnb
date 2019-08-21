bb1f946c4c1191eefa558a341e2dd0be7e074cb5
Dominik Wojciechowski <dominik.woj@me.com>
2019-08-21 15:37:27 +0200

    added container and changed text

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(version: 2019_08_21_101910) do
 
   # These are extensions that must be enabled in order to support this database
@@ -29,9 +26,9 @@ ActiveRecord::Schema.define(version: 2019_08_21_101910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "description"
     t.float "latitude"
     t.float "longitude"
-    t.string "description"
     t.index ["user_id"], name: "index_alpacas_on_user_id"
   end
 
