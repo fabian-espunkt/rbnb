a8796eab9fa287f399d4db3f9483e8a312ceb731
Dominik Wojciechowski <dominik.woj@me.com>
2019-08-22 17:42:43 +0200

    restricted to fucking cities

--- app/javascript/Plugins/init_autocomplete.js
+++ app/javascript/Plugins/init_autocomplete.js
@@ -1,10 +1,17 @@
 import places from 'places.js';
 
+
+
 const initAutocomplete = () => {
   const addressInput = document.getElementById('city-field');
   if (addressInput) {
-    places({ container: addressInput });
+    places({ container: addressInput }).configure({
+      type: 'city',
+      aroundLatLngViaIP: false,
+    });
   }
 };
 
+
 export { initAutocomplete };
+
