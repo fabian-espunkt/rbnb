773c3f68ddba3df194bd99b822f29da513460044
Dominik Wojciechowski <dominik.woj@me.com>
2019-08-23 12:24:11 +0200

    change views to make it work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @alpacas = Alpaca.all
-    @alpacas = current_user.alpacas
+    # @alpacas = current_user.alpacas
   end
 
   def dashboard
